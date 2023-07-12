e6b7812737446c504af456f563ea810e100de2af
JF Abdo <jfabdo@gmail.com>
2023-07-12 16:50:03 -0500

    Added screen scaling

--- Game.py
+++ Game.py
@@ -1,5 +1,15 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+from screeninfo import get_monitors
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        
+        for m in get_monitors():
+            if m.is_primary:
+                primarymonitor = m
+
+        properties = WindowProperties()
+        properties.setSize(primarymonitor.width, primarymonitor.height)
+        self.win.requestProperties(properties)
