c14b39d3b0240612e8f0514a838302d97863c20e
JF Abdo <jfabdo@gmail.com>
2023-07-13 02:38:41 -0500

    made quit functional

--- Game.py
+++ Game.py
@@ -3,6 +3,12 @@ from panda3d.core import WindowProperties
 from screeninfo import get_monitors
 import direct.gui.DirectGui as DirectGui
 from statemachine import StateMachine, State
+from random import choice
+
+welcomescreen = [
+    "Welcome, fool",
+    "Welcome to the dungeons"
+]
 
 class MenuState(StateMachine):
     intro = State("Intro",initial=True) #introduction to game
@@ -21,6 +27,8 @@ class MenuState(StateMachine):
         self.game = Game()
     
     def on_enter_intro(self):
+        self.game.introscreen["text"] = choice(welcomescreen)
+        self.game.introscreen.setText()
         self.game.introscreen.show()
     
     def on_exit_intro(self):
@@ -71,6 +79,10 @@ class Game(ShowBase):
         
     def startGame(self):
         pass
-
-    def quit(self):
+    def cleanup(self):
         pass
+    
+    def quit(self):
+        self.cleanup()
+
+        base.userExit()
